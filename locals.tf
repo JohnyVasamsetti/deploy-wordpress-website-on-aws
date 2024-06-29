46ce4e818be2b864e316266c79ef4dff25ee6c9b
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-06-29 22:58:04 +0530

    [Johny] Create front-tier public subnet & security group

--- locals.tf
+++ locals.tf
@@ -1,6 +1,9 @@
 locals {
   vpc_cidr_block = "10.0.0.0/16"
-  app_tier_cidr_block= "10.0.0.0/24"
+  front_tier_cidr_block = "10.0.0.0/24"
+  app_tier_cidr_block= "10.0.1.0/24"
   region = "us-east-1"
+  ami_id = "ami-0e670eb768a5fc3d4"
+  instance_type = "t2.micro"
   tags = { "task": "wordpress-deploy" }
 }
--- networking.tf
+++ networking.tf
@@ -1,13 +1,45 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = local.vpc_cidr_block
-  tags = {
-    task = "wordpress-deploy"
-    Name = "j03-vpc"
-  }
+  tags = merge(local.tags,{ "Name" : "main-vpc" })
+}
+
+resource "aws_subnet" "front-tier-sn" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = local.front_tier_cidr_block
+  availability_zone = "us-east-1a"
+  tags = merge(local.tags,{"Name":"front-tier-sn"})
 }
 
 resource "aws_subnet" "app-tier-sn" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = local.app_tier_cidr_block
-  tags = merge({"Name":"app-tier-sn"},local.tags)
+  availability_zone = "us-east-1b"
+  tags = merge(local.tags,{"Name":"app-tier-sn"})
+}
+
+resource "aws_security_group" "front-tier-sg" {
+  name   = "front-tier-sg"
+  vpc_id = aws_vpc.main_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    task = "wordpress-deploy"
+    Name = "front-tier-sg"
+  }
 }
