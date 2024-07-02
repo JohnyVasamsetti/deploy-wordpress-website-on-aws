7a93d62dbe0b3b00854db7b5820894e160cca98b
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-07-02 14:16:51 +0530

    [Johny] Add lifecycle policy to the subnets

--- networking.tf
+++ networking.tf
@@ -25,6 +25,9 @@ resource "aws_subnet" "front-tier-sn" {
   cidr_block = local.front_tier_cidr_block
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
+  lifecycle {
+    create_before_destroy = true
+  }
   tags = merge(local.tags,{"Name":"front-tier-sn"})
 }
 
@@ -32,6 +35,9 @@ resource "aws_subnet" "app-tier-sn" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = local.app_tier_cidr_block
   availability_zone = "us-east-1a"
+  lifecycle {
+    create_before_destroy = true
+  }
   tags = merge(local.tags,{"Name":"app-tier-sn"})
 }
 
