d02a3931a1c5ef0f3ce39d52a4bbffd044e343db
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-07-13 00:41:04 +0530

    [Johny] Create elastic file system for apache/wordpress content

---
+++ efs.tf
@@ -0,0 +1,26 @@
+resource "aws_kms_key" "storage_key" {
+  description = "Key for encrypting wordpress file storage"
+  enable_key_rotation = true
+  deletion_window_in_days = 7
+  tags = merge(local.tags,{"Name":"storage_kms"})
+}
+
+resource "aws_efs_file_system" "wordpress-file-storage" {
+  creation_token = "wordpress"
+  encrypted = true
+  kms_key_id = aws_kms_key.storage_key.arn
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+  tags = merge(local.tags,{"Name":"wordpress-storage"})
+}
+
+resource "aws_efs_mount_target" "storage-mt-1" {
+  file_system_id = aws_efs_file_system.wordpress-file-storage.id
+  subnet_id      = aws_subnet.app-tier-sn-1.id
+}
+
+resource "aws_efs_mount_target" "storage-mt-2" {
+  file_system_id = aws_efs_file_system.wordpress-file-storage.id
+  subnet_id      = aws_subnet.app-tier-sn-2.id
+}
--- security_group.tf
+++ security_group.tf
@@ -70,3 +70,19 @@ resource "aws_security_group" "db-tier-sg" {
   }
   tags = merge(local.tags,{"Name":"db-tier-sg"})
 }
+resource "aws_security_group" "storage-tier-sg" {
+  name = "storage-tier-sg"
+  vpc_id = aws_vpc.main_vpc.id
+  ingress {
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
